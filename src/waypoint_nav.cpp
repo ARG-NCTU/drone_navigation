9737221b2f5d793ffd6514c3063df6a56bfc6717
bawkbak <hsiehbawkbak@gmail.com>
2022-06-30 17:19:02 +0800

    Feat: Add action state for behavior tree

--- src/waypoint_nav.cpp
+++ src/waypoint_nav.cpp
@@ -7,98 +7,98 @@ using namespace std;
 
 class Waypoint{
     private:
-    ros::NodeHandle n;
-    ros::Publisher msg_pub;
-    ros::Subscriber msg_sub;
-    ros::Publisher vis_pub;
-    int desired_waypoint[4][3] = {
-        {-2, 5, 10},
-        {-9, 8, 10},
-        {-12, 3, 10},
-        {-16, 1, 10}
-    };
-    float current_pose[3] = {0, 0, 0};
-    int state_num = 0;
-    int state_now = 0;
-    int seq = 1;
-    bool init = false;
-    float margin = 3.0;
+        ros::NodeHandle n;
+        ros::Publisher msg_pub;
+        ros::Subscriber msg_sub;
+        ros::Publisher vis_pub;
+        int desired_waypoint[4][3] = {
+            {-2, 5, 10},
+            {-9, 8, 10},
+            {-12, 3, 10},
+            {-16, 1, 10}
+        };
+        float current_pose[3] = {0, 0, 0};
+        int state_num = 0;
+        int state_now = 0;
+        int seq = 1;
+        bool init = false;
+        float margin = 1.0;
     public:
-    bt::Action action_navigation;
-    Waypoint() : action_navigation("Destinate Landing"){
-        msg_sub = n.subscribe("mavros/local_position/pose", 1000,  &Waypoint::positionCallback, this);
-        msg_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
-        state_num = sizeof(desired_waypoint) / sizeof(*desired_waypoint);
-        
-    }
-
-    void actionSet(int state){
-        switch(state){
-            case 1:
-                action_navigation.set_success();
-                break;
-            case 0:
-                action_navigation.set_running();
-                break;
-            case -1:
-                action_navigation.set_failure();
-                break;
+        bt::Action action_navigation;
+        Waypoint() : action_navigation("Destinate Landing"){
+            msg_sub = n.subscribe("mavros/local_position/pose", 1000,  &Waypoint::positionCallback, this);
+            msg_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
+            state_num = sizeof(desired_waypoint) / sizeof(*desired_waypoint);
+            
         }
-        action_navigation.publish();
-        return;
-    }
 
-    void positionCallback(const geometry_msgs::PoseStamped::ConstPtr& input){
-        current_pose[0] = input->pose.position.x;
-        current_pose[1] = input->pose.position.y;
-        current_pose[2] = input->pose.position.z;
-        return;
-    }
-    void navigation(){
-        // if(state_now == state_num){
-        //     // ros::shutdown();
-        //     actionSet(1);
-        //     return;
-        // }
-        //
-        if (sqrt(pow(abs(-16 - current_pose[0]), 2) + 
-                pow(abs(1 - current_pose[1]), 2)) < margin){
-            // ros::shutdown();
-            actionSet(1);
+        void actionSet(int state){
+            switch(state){
+                case 1:
+                    action_navigation.set_success();
+                    break;
+                case 0:
+                    action_navigation.set_running();
+                    break;
+                case -1:
+                    action_navigation.set_failure();
+                    break;
+            }
+            action_navigation.publish();
             return;
         }
-        actionSet(0);
-        geometry_msgs::PoseStamped position_new;
-        position_new.header.seq = seq;
-        position_new.header.stamp = ros::Time::now();
-        position_new.header.frame_id = "local_origin";
-        position_new.pose.position.x = -16;
-        position_new.pose.position.y = 1;
-        position_new.pose.position.z = 10;
-        msg_pub.publish(position_new);
-        seq++;
-        //
 
-        // if(state_now == 0 && init == false){
-        //     init = true;
-        //     position_new.pose.position.x = desired_waypoint[state_now][0];
-        //     position_new.pose.position.y = desired_waypoint[state_now][1];
-        //     position_new.pose.position.z = 10;
-        //     msg_pub.publish(position_new);
-        //     seq++;
-        // }
-        // if (sqrt(pow(abs(desired_waypoint[state_now][0] - current_pose[0]), 2) + 
-        //         pow(abs(desired_waypoint[state_now][1] - current_pose[1]), 2)) < margin){
-        //     state_now++;
-        //     ROS_INFO("arrived waypoint: %d / %d\n", state_now, state_num);
-        //     position_new.pose.position.x = desired_waypoint[state_now][0];
-        //     position_new.pose.position.y = desired_waypoint[state_now][1];
-        //     position_new.pose.position.z = 10;
-        //     msg_pub.publish(position_new);
-        //     seq++;
-        // }
-        return;
-    }
+        void positionCallback(const geometry_msgs::PoseStamped::ConstPtr& input){
+            current_pose[0] = input->pose.position.x;
+            current_pose[1] = input->pose.position.y;
+            current_pose[2] = input->pose.position.z;
+            return;
+        }
+        void navigation(){
+            // if(state_now == state_num){
+            //     // ros::shutdown();
+            //     actionSet(1);
+            //     return;
+            // }
+            //
+            if (sqrt(pow(abs(desired_waypoint[state_num-1][0] - current_pose[0]), 2) + 
+                    pow(abs(desired_waypoint[state_num-1][1] - current_pose[1]), 2)) < margin){
+                // ros::shutdown();
+                //actionSet(1);
+                return;
+            }
+            //actionSet(0);
+            geometry_msgs::PoseStamped position_new;
+            position_new.header.seq = seq;
+            position_new.header.stamp = ros::Time::now();
+            position_new.header.frame_id = "local_origin";
+            position_new.pose.position.x = desired_waypoint[state_now][0];
+            position_new.pose.position.y = desired_waypoint[state_now][1];
+            position_new.pose.position.z = 10;
+            msg_pub.publish(position_new);
+            seq++;
+            //
+
+            // if(state_now == 0 && init == false){
+            //     init = true;
+            //     position_new.pose.position.x = desired_waypoint[state_now][0];
+            //     position_new.pose.position.y = desired_waypoint[state_now][1];
+            //     position_new.pose.position.z = 10;
+            //     msg_pub.publish(position_new);
+            //     seq++;
+            // }
+            if (sqrt(pow(abs(desired_waypoint[state_now][0] - current_pose[0]), 2) + 
+                    pow(abs(desired_waypoint[state_now][1] - current_pose[1]), 2)) < margin){
+                state_now++;
+                ROS_INFO("arrived waypoint: %d / %d\n", state_now, state_num);
+                // position_new.pose.position.x = desired_waypoint[state_now][0];
+                // position_new.pose.position.y = desired_waypoint[state_now][1];
+                // position_new.pose.position.z = 10;
+                // msg_pub.publish(position_new);
+                // seq++;
+            }
+            return;
+        }
 };
 
 int main(int argc, char **argv){
@@ -106,6 +106,14 @@ int main(int argc, char **argv){
     Waypoint waypoint;
     while(ros::ok()){
         if(waypoint.action_navigation.is_active() && waypoint.action_navigation.active_has_changed()){
+            ROS_INFO("Action: Nav activiate");
+        }
+        if(waypoint.action_navigation.active_has_changed() && !(waypoint.action_navigation.is_active())){
+            ROS_INFO("done");
+            waypoint.actionSet(1);
+        }
+        if(waypoint.action_navigation.is_active()){
+            waypoint.actionSet(0);
             waypoint.navigation();
         }
         ros::spinOnce();
--- src/waypoint_tsp.cpp
+++ src/waypoint_tsp.cpp
@@ -2,93 +2,139 @@
 #include "math.h" 
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "std_msgs/Bool.h"
 #include "ros_tsp/tspGoalpoint.h"
+#include <behavior_tree/behavior_tree.h>
 using namespace std;
 ros_tsp::tspGoalpoint msg;
 
 class Waypoint{
     private:
-    ros::NodeHandle n;
-    ros::Publisher msg_pub_subgoal;
-    ros::Subscriber msg_sub_tsp;
-    ros::Subscriber msg_sub_pose;
-    // int desired_waypoint[3][2] = {
-    //     {10, 20},
-    //     {10, 30},
-    //     {10, 10},
-    // };
-    int state_num = 0;
-    int state_now = 0;
-    int seq = 1;
-    bool init = false;
-    float margin = 1.0;
-    float** desired_waypoint;
+        ros::NodeHandle n;
+        ros::Publisher msg_pub_subgoal;
+        ros::Publisher msg_pub_state;
+        ros::Subscriber msg_sub_tsp;
+        ros::Subscriber msg_sub_pose;
 
+        int state_num = 0;
+        int state_now = 0;
+        int seq = 1;
+        bool init_tsp = false;
+        bool state_tsp = false;
+        float margin = 1.0;
+        float** desired_waypoint;
+        float desired_height = 0;
+        float current_pose[3] = {0, 0, 0};
     public: 
-    Waypoint(){
-        msg_sub_tsp = n.subscribe("tsp_goalpoint", 1000,  &Waypoint::waypointCallback, this);
-        msg_sub_pose = n.subscribe("mavros/local_position/pose", 1000,  &Waypoint::positionCallback, this);
-        msg_pub_subgoal = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
-        // state_num = sizeof(desired_waypoint) / sizeof(*desired_waypoint);
-    }
-    void positionCallback(const geometry_msgs::PoseStamped::ConstPtr& input){
-        geometry_msgs::PoseStamped position_new;
-        position_new.header.frame_id = "local_origin";
-        if(init == false){return;}
-        if(state_now == state_num){
-            while(ros::ok()){
-                ROS_INFO("TSP Finished");
-                position_new.header.seq = seq;
-                position_new.header.stamp = ros::Time::now();
-                position_new.pose.position.x = desired_waypoint[state_now][0];
-                position_new.pose.position.y = desired_waypoint[state_now][1];
-                msg_pub_subgoal.publish(position_new);
-                seq++;
-            }
-            return;
-        }
-        
-        if (sqrt(pow(abs(desired_waypoint[state_now][0] - input->pose.position.x), 2) + 
-                pow(abs(desired_waypoint[state_now][1] - input->pose.position.y), 2)) < margin){
-            state_now++;
-            ROS_INFO("arrived waypoint: %d / %d\n", state_now, state_num);
-            position_new.pose.position.x = desired_waypoint[state_now][0];
-            position_new.pose.position.y = desired_waypoint[state_now][1];
-            msg_pub_subgoal.publish(position_new);
-            seq++;
-        }
-        return;
-    }
-
-    void waypointCallback(const ros_tsp::tspGoalpoint::ConstPtr& input){
-        if(init == false){
-            init = true;
-            state_num = input->data_x.size();
-            ROS_INFO("number of waypoint: %d\n", state_num);
-            desired_waypoint = new float*[state_num];
-            for(int i=0; i<state_num; i++){
-                desired_waypoint[i] = new float[2];
-            }
-            for(int i=0; i<state_num; i++){
-                desired_waypoint[i][0] = input->data_x[i];
-                desired_waypoint[i][1] = input->data_y[i];
-            }
-        }
-        // for(int i=0; i<state_num; i++){
-        //     for(int j=0; j<2; j++){
-        //         cout << desired_waypoint[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        // cout <<"size : "<< sizeof(desired_waypoint) / sizeof(*desired_waypoint) << endl;
-        // cout <<"size : "<< sizeof(desired_waypoint[0]) / sizeof(float) << endl;        
-        return;
-    }
+        bt::Action action_tsp;
+        Waypoint();
+        void positionCallback(const geometry_msgs::PoseStamped::ConstPtr& input);
+        void waypointCallback(const ros_tsp::tspGoalpoint::ConstPtr& input);
+        void actionSet(int state);
+        void navigation();
+        void stateCheck();
 };
 
+Waypoint :: Waypoint() : action_tsp("Destinate TSP"){
+    msg_sub_tsp = n.subscribe("tsp_goalpoint", 1000,  &Waypoint::waypointCallback, this);
+    msg_sub_pose = n.subscribe("mavros/local_position/pose", 1000,  &Waypoint::positionCallback, this);
+    msg_pub_subgoal = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
+    msg_pub_state = n.advertise<std_msgs::Bool>("tsp_finished", 10);
+}
+
+void Waypoint :: positionCallback(const geometry_msgs::PoseStamped::ConstPtr& input){
+    current_pose[0] = input->pose.position.x;
+    current_pose[1] = input->pose.position.y;
+    current_pose[2] = input->pose.position.z;
+    n.getParam("drone/desired_height", desired_height);
+    return;
+}
+
+void Waypoint :: waypointCallback(const ros_tsp::tspGoalpoint::ConstPtr& input){
+    if(init_tsp == false){
+        init_tsp = true;
+        state_num = input->data_x.size();
+        ROS_INFO("number of waypoint: %d\n", state_num);
+        desired_waypoint = new float*[state_num];
+        for(int i=0; i<state_num; i++){
+            desired_waypoint[i] = new float[2];
+        }
+        for(int i=0; i<state_num; i++){
+            desired_waypoint[i][0] = input->data_x[i];
+            desired_waypoint[i][1] = input->data_y[i];
+        }
+    }
+    return;
+}
+
+
+
+void Waypoint :: actionSet(int state){
+    switch(state){
+        case 1:
+            action_tsp.set_success();
+            break;
+        case 0:
+            action_tsp.set_running();
+            break;
+        case -1:
+            action_tsp.set_failure();
+            break;
+    }
+    action_tsp.publish();
+    return;
+}
+
+void Waypoint :: navigation(){
+    if(init_tsp == false){return;}
+    if(state_now == state_num){
+        state_tsp = true;
+        actionSet(1);
+        return;
+    }
+    geometry_msgs::PoseStamped position_new;
+    position_new.header.seq = seq;
+    position_new.header.stamp = ros::Time::now();
+    position_new.header.frame_id = "local_origin";
+    position_new.pose.position.x = desired_waypoint[state_now][0];
+    position_new.pose.position.y = desired_waypoint[state_now][1];
+    position_new.pose.position.z = desired_height;
+    msg_pub_subgoal.publish(position_new);
+    seq++;
+    
+    if (sqrt(pow(abs(desired_waypoint[state_now][0] - current_pose[0]), 2) + 
+            pow(abs(desired_waypoint[state_now][1] - current_pose[1]), 2)) < margin){
+        state_now++;
+        ROS_INFO("arrived waypoint: %d / %d\n", state_now, state_num);
+    }
+    return;
+}
+
+void Waypoint :: stateCheck(){
+    std_msgs::Bool tsp_state;
+    tsp_state.data = state_tsp;
+    msg_pub_state.publish(tsp_state);
+    return;
+}
+
 int main(int argc, char **argv){
-    ros::init(argc, argv, "drone_nav");
+    ros::init(argc, argv, "drone_tsp");
     Waypoint waypoint;
-    ros::spin();
+    while(ros::ok()){
+        if(waypoint.action_tsp.is_active() && waypoint.action_tsp.active_has_changed()){
+            ROS_INFO("Action: Nav TSP activiate");
+        }
+        if(waypoint.action_tsp.active_has_changed() && !(waypoint.action_tsp.is_active())){
+            ROS_INFO("Action: Done");
+            waypoint.actionSet(1);
+        }
+        if(waypoint.action_tsp.is_active()){
+            waypoint.actionSet(0);
+            waypoint.navigation();
+            //ros::Duration(0.5).sleep(); 
+        }
+        waypoint.stateCheck();
+        ros::spinOnce();
+    }
     return 0;
 }
